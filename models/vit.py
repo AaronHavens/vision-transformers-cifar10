8b8f2969f124d94eabde906c943f80b5f138ca5f
AaronHavens <ahavens2@illinois.edu>
2023-08-25 16:49:17 -0500

    center norm

--- models/vit.py
+++ models/vit.py
@@ -16,7 +16,7 @@ def pair(t):
 class PreNorm(nn.Module):
     def __init__(self, dim, norm_fn, net_fn):
         super().__init__()
-        self.norm = norm_fn(dim)#nn.LayerNorm(dim)
+        self.norm = norm_fn#nn.LayerNorm(dim)
         self.fn = fn
     def forward(self, x, **kwargs):
         return self.fn(self.norm(x), **kwargs)
